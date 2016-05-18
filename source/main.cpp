e78fdec3a2192beb9839dbac6047610c26af8281
bakeunbi <bakeunbi1@gmail.com>
2016-05-18 20:43:28 +0900

    usage

--- source/main.cpp
+++ source/main.cpp
@@ -12,53 +12,78 @@
 using namespace std;
 
 int main(int argc, char** argv){
-	int pSize=3, K=3, knn=1;
-	//string RP = "yes";
-	string run = "eval";
-	//patchTextons.exe data.rat K pSize RP_Type	//image_type
+
+	string fname = argv[1];
+	if (fname == "help"){
+		cout << "<Usage> \n"
+			<<"Train: patchTextons.exe filename(.rat) train k fsize \n"
+			<< "Test: patchTextons.exe filename(.rat) test k pSize knn\n\n"
+			<< "<Parameter description>\n"
+			<< "k: the parameter deciding the number of center in each class of a fold\n"
+			<< "\t(Finally, the number of textons is 4fold x 5 classes x k = 20k)\n"
+			<< "fsize: the parameter deciding the feature dimension\n"
+			<< "\t(Finally, feature dimension is fsize x fsize\n"
+			<< "pSize: the parameter deciding the patch size for learning histogram\n"
+			<< "\t(Finally, the patch size is pSize x pSize.\n"
+			<< "knn: the parameter for k-nn\n\n"
+			<< "<default parameter setting>\n"
+			<< "k = 3, pSize = 3, knn = 1\n";
+	}
+
+	if (argc > 2){
+		PTexton pTexton = PTexton();
+		int pSize = 3, K = 3, knn = 1;
+
+		string run = argv[2];
+		
+		if (run == "train"){
+			if (argc > 4){
+				K = atoi(argv[3]);
+				pSize = atoi(argv[4]);
+				pTexton.initialize(fname, pSize, K, knn);	//file name, (fsize or pSize), K,knn
+				pTexton.learningTexton();
+				pTexton.train();
+			}
+			else{
+				cout << "Usage Error: \"help\"" << endl;
+				return 1;
+			}
+		}
+		//else if (run == "gray"){
+		//	if (argc > 4){
+		//		K = atoi(argv[3]);
+		//		pSize = atoi(argv[4]);
+		//		pTexton.initialize(fname, pSize, K, knn);	//file name, (fsize or pSize), K,knn
+		//		pTexton.grayscaleTexton();
+		//	}
+		//	else{
+		//		cout << "Usage Error: \"help\"" << endl;
+		//		return 1;
+		//	}
+		//	
+		//}
+		else if (run == "test"){
+			if (argc > 5){
+				K = atoi(argv[3]);
+				pSize = atoi(argv[4]);
+				knn = atoi(argv[5]);
+
+				pTexton.initialize(fname, pSize, K, knn);	//file name, (fsize or pSize), K,knn
+				pTexton.test();
+				pTexton.errorAssessment();
+			}
+			else{
+				cout << "Usage Error: \"help\"" << endl;
+				return 1;
+			}
+		}
+	}
 
 	if (argc < 2){
 		cout << "Usage Error: Select image!" << endl;
 		return 1;
 	}
-	else if (argc < 4){
-		K = atoi(argv[2]);
-	}
-	else if (argc < 5){
-		K = atoi(argv[2]);
-		pSize = atoi(argv[3]);
-	}
-	else if (argc < 6){
-		K = atoi(argv[2]);
-		pSize = atoi(argv[3]);
-		 run = argv[4];
-	}
 
-	string fname = argv[1];
-	PTexton pTexton = PTexton();
-	pTexton.initialize(fname, pSize, K, knn);	//file name, patch size, K,RP
-		
-	if (run == "eval"){
-		pTexton.evaluate();
-	}
-	else if (run == "train"){
-		pTexton.learningTexton();
-		pTexton.train();
-	}
-	else if (run == "test"){
-		pTexton.test();
-		pTexton.errorAssessment();
-	}
-	else if (run == "assess"){
-		pTexton.errorAssessment();
-	}
-	//else if (run == "print"){
-	//	//pTexton.printTextonMap();
-	//	pTexton.printResult();
-	//}
-	else if (run == "gray"){
-		pTexton.grayscaleTexton();
-	}
 	return 0;
 }
 
